5610126587349d3ca3a314ec2c3c12c738e60265
unknown <daniel0107borges@gmail.com>
2024-12-20 20:59:16 -0300

    finalizacao e teste

--- src/bancoexercicio/ContaBanco.java
+++ src/bancoexercicio/ContaBanco.java
@@ -9,6 +9,16 @@ public class ContaBanco {
     private boolean status;
 
     //Métodos personalizados 
+    
+    public void estadoAtual(){
+        System.out.println("===========================");
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono:" + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.getStatus());
+    }    
+    
     public void abrirConta(String t) {
         this.setTipo(t);
         this.setStatus(true);
@@ -36,15 +46,38 @@ public class ContaBanco {
         if(this.getStatus()){
             //this.saldo = this.saldo + v;
             this.setSaldo( this.getSaldo() + v);
+            System.out.println("Deposito realizado na conta de " + this.getDono());
+        }else{
+            System.out.println("Impossivel em depositar em uma conta fechada!");
         }
     }
 
-    public void sacar() {
-
+    public void sacar(float v) {
+        if(this.getStatus()){
+            if(this.getSaldo()>= v){
+              this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saque realizado na conta de " + this.getDono());
+            }else{
+                System.out.println("Saldo insuficiente para saque");
+            }
+        }else{
+            System.out.println("Impossivel sacar de uma conta fechada");
+        }
     }
 
     public void pagarMensal() {
-
+        int v = 0;
+        if (this.getTipo() == "CC") {
+            v = 12;
+        } else if (this.getTipo()== "CP") {
+            v = 20;
+        }
+        if(this.getStatus()){
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mensalidad paga com sucesso por " + this.getDono());
+        }else{
+            System.out.println("Impossivel pagar uma conta fechada");
+        }
     }
 
     //Metodos especias 
